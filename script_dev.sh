d372672ce7be2d035fab29e429b613f920e13dc4
NandaRusfikri <nandarusfikri@gmail.com>
2023-07-17 15:41:38 +0700

    versi 1.0.9

--- script_dev.sh
+++ script_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo fuser -n tcp -k 54321
 mv rest-api /root/rest-api/rest-api
-nohup /root/rest-api/rest-api > /root/rest-api/api.log 2>&1 &
+nohup /root/rest-api/rest-api > api.log 2>&1 &
